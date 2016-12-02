a1f450e3b80bdea43218d5cf050f79f0126f599b
Georg Haas <hax404foogit@hax404.de>
2016-12-02 21:12:27 +0100

    first working prototype

--- dockin.sh
+++ dockin.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+sleep 1
+DISPLAY=:0 su georg -c "xrandr --output LVDS1 --off"
+DISPLAY=:0 su georg -c "xrandr --output HDMI3 --mode 1280x1024"
+DISPLAY=:0 su georg -c "xrandr --output HDMI2 --mode 1280x1024 --right-of HDMI3 --rotate left"
+DISPLAY=:0 su georg -c "xrandr --dpi 96"
 
-xrandr --output LVDS1 --off
-xrandr --output HDMI3 --mode 1280x1024
-xrandr --output HDMI2 --mode 1280x1024 --right-of HDMI3 --rotate left
-
-sudo -E systemctl stop openvpn@dn42-chero.service
-sudo -E systemctl stop openvpn@ham-db0fhn
-sudo -E systemctl stop netctl-auto@wlp3s0
-sudo -E netctl start enp0s25-dhcp
+/usr/bin/systemctl stop openvpn@dn42-chero.service
+/usr/bin/systemctl stop openvpn@ham-db0fhn.service
+/usr/bin/systemctl stop netctl-auto@wlp3s0.service
+/usr/bin/netctl start enp0s25-dhcp
 
--- dockout.sh
+++ dockout.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-xrandr --output HDMI2 --off
-xrandr --output HDMI3 --off
-xrandr --output LVDS1 --auto
+sleep 1
+DISPLAY=:0 su georg -c "xrandr --output HDMI2 --off"
+DISPLAY=:0 su georg -c "randr --output HDMI3 --off"
+DISPLAY=:0 su georg -c "xrandr --output LVDS1 --auto"
 
-sudo -E netctl stop enp0s25-dhcp
-sudo -E systemctl start netctl-auto@wlp3s0
-sudo -E systemctl start openvpn@dn42-chero.service
-sudo -E systemctl start openvpn@ham-db0fhn
+/usr/bin/netctl stop enp0s25-dhcp
+/usr/bin/systemctl start netctl-auto@wlp3s0.service
+/usr/bin/systemctl start openvpn@dn42-chero.service
+/usr/bin/systemctl start openvpn@ham-db0fhn.service
 
